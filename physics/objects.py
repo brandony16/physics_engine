c8d15f04a1f8b6a24e71b42c40b0da8532f218f9
brandony16 <108242812+brandony16@users.noreply.github.com>
2025-07-10 15:35:25 -0400

    Add some more object constructors

--- physics/objects.py
+++ physics/objects.py
@@ -1,11 +1,33 @@
+import numpy as np
+from typing import Sequence
+
+
 class Object:
-    def __init__(self, mass: float, position: tuple, velocity: float):
+    def __init__(
+        self,
+        mass: float,
+        position: np.ndarray | Sequence[float],
+        velocity: np.ndarray | Sequence[float],
+    ):
+        if mass <= 0:
+            raise ValueError("Mass must be a positive, non-zero number")
+        if len(position) != 2:
+            raise ValueError("Position must be a 2D vector")
+        if len(velocity) != 2:
+            raise ValueError("Velocity must be a 2D vector")
+
         self.mass = mass
-        self.position = position
-        self.velocity = velocity
+        self.position = np.asarray(position, dtype=float)
+        self.velocity = np.asarray(velocity, dtype=float)
 
 
 class Circle(Object):
-    def __init__(self, radius: int):
-        super.__init__()
+    def __init__(
+        self,
+        mass: float,
+        position: np.ndarray | Sequence[float],
+        velocity: np.ndarray | Sequence[float],
+        radius: float,
+    ):
+        super().__init__(mass, position, velocity)
         self.radius = radius
---
+++ tests/object_test.py
@@ -0,0 +1,6 @@
+import unittest
+from physics.objects import Object, Circle;
+
+class ObjectTest(unittest.TestCase):
+  def invalidInputs(self):
+    pass
