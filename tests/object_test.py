5aaf443d9a5b9262cec390dc5f665b1688419491
brandony16 <108242812+brandony16@users.noreply.github.com>
2025-07-11 16:59:45 -0400

    Add setup.py file to make project a package

--- tests/object_test.py
+++ tests/object_test.py
@@ -1,6 +1,11 @@
 import unittest
-from physics.objects import Object, Circle;
+from physics.objects import Object, Circle
+
 
 class ObjectTest(unittest.TestCase):
-  def invalidInputs(self):
-    pass
+    def invalidInputs(self):
+        self.assertEqual(1, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
